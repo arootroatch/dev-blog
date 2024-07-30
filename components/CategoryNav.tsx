facacf73f82702abf580f6df22f2870f4df93159
Alex Root-Roatch <arootroatch@gmail.com>
2024-07-30 09:05:00 -0500

    fix bug in category nav links

--- components/CategoryNav.tsx
+++ components/CategoryNav.tsx
@@ -2,14 +2,16 @@ import {Posts} from "@/lib/interfaces";
 import Link from "next/link";
 import styles from "./categoryNav.module.css"
 
-export default function CategoryNav({categorized}: {categorized: Posts[][]}) {
-    return <nav className={styles.nav}>
-        <ul className={styles.ul}>
-            {categorized.map((category) =>
-              (<li className={styles.li}
-                   key={category[0].data.category[0]}>
-                  <Link href={`./#${category[0].data.category}`}>{category[0].data.category[0]}</Link>
-              </li>))}
-        </ul>
-    </nav>;
+export default function CategoryNav({categorized}: { categorized: Posts[][] }) {
+  return <nav className={styles.nav}>
+    <ul className={styles.ul}>
+      {categorized.map((category) =>
+        (<li className={styles.li}
+             key={category[0].data.category[0]}>
+            <Link href={`./#${category[0].data.category[0]}`}>{category[0].data.category[0]}</Link>
+          </li>
+        ))}
+    </ul>
+  </nav>
+    ;
 }
