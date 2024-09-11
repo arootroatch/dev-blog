29777051789a984714770747728aaff49bd2b1c8
Alex Root-Roatch <arootroatch@gmail.com>
2024-09-11 03:30:11 -0500

    add rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ import withMDX from '@next/mdx'
 
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  async rewrites(){
+    return {
+      beforeFiles: [
+        {
+          source: '/ttt',
+          destination: '/ttt/index.html',
+        },
+      ],
+    }
+  }
 };
 
 export default withMDX(nextConfig);
